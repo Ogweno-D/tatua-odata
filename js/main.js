8f6ccc7cd1fa84c7e03162a9c0cc6b6aa258d323
Ogweno-D <dennisogwene@gmail.com>
2025-09-01 12:39:11 +0300

    Reusability test!

--- js/main.js
+++ js/main.js
@@ -44,3 +44,41 @@ const peopleTableConfig = {
 const peopleTable = new ReusableTableFromApi(
     "peopleTable", fetchPeopleFromApi, peopleTableConfig);
 
+const people2TableConfig = {
+    columns: [
+        TableColumn({
+            id: "UserName",
+            caption: "Username",
+            isFilterable: true,
+            isSortable: true
+        }),
+        TableColumn({
+            id: "FirstName",
+            caption: "First Name",
+            isFilterable: true,
+            isSortable: true
+        }),
+        TableColumn({
+            id: "LastName",
+            caption: "Last Name",
+            isFilterable: true,
+            isSortable: true
+        }),
+        TableColumn({
+            id: "Gender",
+            caption: "Gender",
+            isFilterable: true,
+            enumValues: ["Male", "Female", ]
+        }),
+        TableColumn({
+            id: "Emails",
+            caption: "Email",
+            isFilterable: true,
+            isSortable: true,
+            hide: true
+        })
+    ]
+};
+const peopleTable2 = new ReusableTableFromApi(
+    "people2Table", fetchPeopleFromApi, people2TableConfig);
+
